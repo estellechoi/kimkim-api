bf274fb5b3ba0292fb348a4928ae22df3da8b064
estellechoi <estele.choi@gmail.com>
2024-05-22 22:34:42 +1000

    feat: add micro change to run actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { NextPage } from 'next/types';
 const Home: NextPage = () => {
   return (
     <main role="main" className="flex flex-col items-center gap-y-20 min-h-screen pt-app_header_height pb-page_bottom">
-      <AppLogo size="lg" />
+      <AppLogo size="lg" className="mt-14" />
 
       <span className="flex items-baseline gap-x-2">
         <span className="Font_caption_sm">Visit KimKim at</span>
