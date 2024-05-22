8cb6658bcae949ec7c7a9ffc5ee43f734fc30f15
estellechoi <estele.choi@gmail.com>
2024-05-22 22:39:42 +1000

    feat: add micro change to run actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     <main role="main" className="flex flex-col items-center gap-y-20 min-h-screen pt-app_header_height pb-page_bottom">
       <AppLogo size="lg" className="mt-14" />
 
-      <span className="flex items-baseline gap-x-2">
+      <span className="flex items-baseline gap-x-3">
         <span className="Font_caption_sm">Visit KimKim at</span>
         <a className="inline Font_body_sm hover:text-semantic_info hover:underline" href="https://kimkim.app">
           https://kimkim.app
